cc43ed067a79c6215e36922cec8dc44b425c51b9
Alex <alexandre.crooij@students.hevs.ch>
2018-05-18 13:53:50 +0200

    Initial commit

---
+++ Agricathon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Agricathon.Startup))]
+namespace Agricathon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
